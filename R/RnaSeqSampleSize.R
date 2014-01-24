6e8775060e69c420cf8fd1fd7415d6ec7c64eefb
slzhao <zhaoshilin@gmail.com>
2014-01-24 16:04:36 -0600

    V1.1.0

---
+++ R/RnaSeqSampleSize.R
@@ -0,0 +1,142 @@
+est_pvalue_store<-function(X1X0Sum, n, phi, w=1) {
+	a<-0:X1X0Sum
+	if(w == 1){
+		anom<-dnbinom(a, mu=X1X0Sum/2, size=n/phi)
+		zanom<-rev(anom)
+	}else{
+		anom<-dnbinom(a, mu=(w*X1X0Sum/(w+1)), size=n/phi)
+		zanom<-dnbinom( X1X0Sum-a, mu=(X1X0Sum/(w+1)), size=n/phi)
+	}
+	azanom<-anom * zanom
+	yy<-azanom/sum(azanom)
+	return(yy)
+}
+
+est_pvalue_store_get_power3<-function(x1,x0,yy) {
+	yy<-yy[[(x1+x0+1)]]
+	if (x1>=x0) {
+		pvalue<-2* sum(yy[(x1+1):length(yy)])
+	} else {
+		pvalue<-2*sum(yy[1:(x1+1)])
+	}
+	return(min(pvalue, 1))
+}
+est_pvalue_store_get_power3_m<-Vectorize(est_pvalue_store_get_power3, vectorize.args=c("x1", "x0"))
+
+est_power3<-function(n, w=1.0, rho=2.0, lambda0=5, phi_0=1, beta=0.2, alpha=0.05, error=0.001){
+	mu0<-lambda0
+	mu1<-mu0*(rho*w)
+	phi_1<-phi_0
+	q0_u<-qnbinom(1-error, size=n/phi_0, mu=n*mu0)
+	q0_l<-qnbinom(error, size=n/phi_0, mu=n*mu0)
+	q1_u<-qnbinom(1-error, size=n/phi_1, mu=n*mu1)
+	q1_l<-qnbinom(error, size=n/phi_1, mu=n*mu1)
+	
+	yMin<-min(q1_l,q1_u)+min(q0_l,q0_u)
+	yMax<-max(q1_l,q1_u)+max(q0_l,q0_u)
+	yyStore<-list()
+	for (y in yMin:yMax) {
+		yyStore[[y+1]]<-est_pvalue_store(y,n=n, phi=phi_0, w=w)
+	}
+	a<-outer(q1_l:q1_u, q0_l:q0_u, est_pvalue_store_get_power3_m,yy=yyStore) <= alpha
+	
+	temp<-dnbinom(q1_l:q1_u, mu=(n*mu1), size=n/phi_1)
+	b<-apply(a, 2, function(x) x*temp )
+	temp<-dnbinom(q0_l:q0_u, mu=(n*mu0), size=n/phi_0)
+	power<-sum( apply(b, 1, function(x) x*temp ) )
+	return(power-(1-beta))
+}
+
+##' sample_size
+##' 
+##' A function to estitamete the sample size for differential expression analysis of RNA-seq data.
+##' 
+##' A function to estitamete the sample size for differential expression analysis of RNA-seq data.
+##' 
+##' @param m Total number of genes for testing.
+##' @param m1 Expected number of prognostic genes.
+##' @param power Power to detecte prognostic genes.
+##' @param f FDR level
+##' @param w Ratio of normalization factors between two groups.
+##' @param rho minimum fold changes for prognostic genes between two groups.
+##' @param lambda0 Average read counts for prognostic genes.
+##' @param phi_0 Dispersion for prognostic genes.
+##' @param nTable A table included the estitameted sample numbers (N) with different parameters.
+##' @export
+##' @examples #Input initial parameters. These parameters were stored in our N table so you will get results in one second.
+##' vm<-10000;vm1<-100;vpower<-0.8;vf<-0.05;vw<-1.0;vrho<-2.0;vlambda0<-5;vphi_0<-2
+##' sample_size(m=vm, m1=vm1, power=vpower, f=vf, w=vw, rho=vrho, lambda0=vlambda0, phi_0=vphi_0)
+##' #Estitamete sample size for parameters not stored, may use two minutes.
+##' vm<-30000;vm1<-1000;vpower<-0.8;vf<-0.05;vw<-1.0;vrho<-1.5;vlambda0<-5;vphi_0<-0.5
+##' sample_size(m=vm, m1=vm1, power=vpower, f=vf, w=vw, rho=vrho, lambda0=vlambda0, phi_0=vphi_0)
+sample_size<-function(m=10000, m1=100, power=0.8, f=0.1, w=1, rho=2, lambda0=5, phi_0=1,nTable=system.file("extdata", "nTable.csv", package = "RnaSeqSampleSize")){
+	r1<-m1 * power
+	beta<-1-power
+	
+	alpha_star<-r1*f/((m-m1)*(1-f))
+	z_alpha<-qnorm(1-alpha_star/2, lower.tail=T)
+	z_beta<-qnorm(power, lower.tail=T)
+	n_w<-( ( z_alpha + z_beta )^2* (1 + rho/w +phi_0*lambda0*(1+rho^2)) )/ ( (rho-1)^2*lambda0 )
+	end.point<-round(n_w)+30
+#	require("ssanv")
+	
+	nTable<-read.csv(nTable,header=T)
+	n_near<-find_near_N(nTable,rho=rho,phi0=phi_0,lambda0=lambda0,f=f,m=m, m1=m1, power=power)
+	if (length(n_near)!=0) {
+		if (length(grep("M",n_near))!=0) {
+			n_Exact<-as.numeric(gsub("M","",n_near))
+			return(n_Exact)
+		} else {
+			start.point<-n_near
+		}
+	} else {
+		start.point<-1
+	}
+	p1<-est_power3(n=start.point,w=w, rho=rho, lambda0=lambda0, phi_0=phi_0, beta=beta, alpha=alpha_star)
+	if (p1>0) {
+		return(start.point)
+	} else {
+		n_Exact<-uniroot.integer(est_power3, c(start.point, end.point), w=w, rho=rho, 
+				lambda0=lambda0, phi_0=phi_0, beta=beta, alpha=alpha_star, pos.side=T,
+				step.up=T, step.power=4)$root  
+		return(n_Exact)
+	}
+}
+
+find_near<-function(allData,input,method=c("smaller","larger")) {
+	if (input %in% allData) {
+		return(input)
+	}
+	method<- if (missing(method)) "smaller" else match.arg(method)
+	if (method=="smaller") {
+		allData<-allData[which(allData<=input)]
+	} else {
+		allData<-allData[which(allData>=input)]
+	}
+	if (length(allData>0)) {
+		return(allData[which.min(abs(allData-input))])
+	} else {return(NA)}
+}
+
+find_near_N<-function(nTable,rho,phi0,lambda0,f,power,m,m1) {
+	rho_near<-find_near(nTable[,1],rho,"larger")
+	phi0_near<-find_near(nTable[,2],phi0,"smaller")
+	lambda0_near<-find_near(nTable[,3],lambda0,"larger")
+	f_near<-find_near(nTable[,4],f,"larger")
+	power_near<-find_near(nTable[,5],power,"larger")
+	m_near<-find_near(nTable[,6],m,"smaller")
+	m1_near<-find_near(nTable[,7],m1,"larger")
+	n_near<-nTable[which(nTable[,1]==rho_near & nTable[,2]==phi0_near & nTable[,3]==lambda0_near & nTable[,4]==f_near & nTable[,5]==power_near & nTable[,6]==m_near & nTable[,7]==m1_near),8]
+	if (as.character(rho_near)==as.character(rho) & as.character(phi0_near)==as.character(phi0) & as.character(lambda0_near)==as.character(lambda0) & as.character(f_near)==as.character(f) & as.character(power_near)==as.character(power) & as.character(m_near)==as.character(m) & as.character(m1_near)==as.character(m1)) {
+		n_near<-paste("M",n_near,sep="")
+	} else {
+		estTime<-as.integer(nTable[which(nTable[,1]==rho_near & nTable[,2]==phi0_near & nTable[,3]==lambda0_near & nTable[,4]==f_near & nTable[,5]==power_near & nTable[,6]==m_near & nTable[,7]==m1_near),9])
+		if (length(estTime)==0) {
+			cat("RnaSeqSampleSize can't find nearest sample size in stored N table. Will estimate sample size N from N=1.\n")
+		} else {
+			cat(paste("RnaSeqSampleSize will use about ",estTime," seconds to estimate sample number N.\n",sep=""))
+		}
+		flush.console()
+	}
+	return(n_near)
+}
--- README.md
+++ README.md
@@ -1,2 +1,83 @@
 RnaSeqSampleSize
-================
+============
+* [Introduction](#Introduction)
+* [Change log](#Change)
+* [Download and install](#download)
+* [Example](#example)
+* [Usage](#usage)
+
+<a name="Introduction"/>
+# Introduction #
+Sample size calculation is an important issue in the experimental design of biomedical research. For RNA-seq experiments, the sample size calculation method based on the Poisson model has been proposed; however, when there are biological replicates, RNA-seq data could exhibit variation significantly greater than the mean (i.e. over-dispersion). The Poisson model cannot appropriately model the over-dispersion, and in such cases, the negative
+binomial model has been used as a natural extension of the Poisson model. Because the field currently lacks a sample size calculation method based on the negative binomial model for assessing differential expression analysis of RNA-seq data, we propose a method to calculate the sample size.
+
+RnaSeqSampleSize package is based on the paper **Sample size calculation based on exact test for assessing differential expression analysis in RNA-seq data**, which was published in BMC Bioinformatics. The paper can be accessed in [PubMed](http://www.ncbi.nlm.nih.gov/pubmed/?term=24314022).
+
+<a name="Change"/>
+# Change log #
+
+2014-01-24
+RnaSeqSampleSize 1.1.0:
+ * A more detail parameter to sample size table was provided to make sample size estimation faster;
+ * An estimated time to perform sample size estimation will be displayed;
+ * A bug fixed when sample size N equals to 1;
+
+2014-01-10
+RnaSeqSampleSize 1.0.0:
+ * First version;
+
+<a name="download"/>
+# Download and install #
+**You can directly download the zip file (Windows system) or tar.gz file (Linux system) of RnaSeqSampleSize package** from [github release page](https://github.com/slzhao/RnaSeqSampleSize/releases).
+
+Or you can download the source codes of RnaSeqSampleSize package from [github](https://github.com/slzhao/RnaSeqSampleSize) by the following commands (If git has already been installed in your computer). And then you need to built the source codes into a package.
+
+	#This is not necessary if you have already downloaded the zip or tar.gz file from release page
+	#The source codes of RnaSeqSampleSize package will be downloaded to your current directory
+	git clone https://github.com/slzhao/RnaSeqSampleSize.git
+	#Built the source codes into a package
+	R CMD build RnaSeqSampleSize
+	#The file of RnaSeqSampleSize package will be generated in current folder
+
+RnaSeqSampleSize package requires ssanv packages. You can enter R and use following R codes to install it. 
+
+	#Install required CRAN packages
+	install.packages("ssanv")
+
+Then you can enter R and use following R codes to install RnaSeqSampleSize in Windows or Linux.
+	
+	#Install RnaSeqSampleSize in Windows system, assume RnaSeqSampleSize_1.1.0.zip file was in current directory
+	install.packages("RnaSeqSampleSize_1.1.0.zip")
+	#Install RnaSeqSampleSize in Linux system, assume RnaSeqSampleSize_1.1.0.tar.gz file was in current directory
+	install.packages("RnaSeqSampleSize_1.1.0.tar.gz")
+
+<a name="example"/>
+# Example #
+After you have installed RnaSeqSampleSize package. You can enter R and use following R codes to see the examples for it.
+	
+	#Load package
+	library("RnaSeqSampleSize")
+	#View help files
+	?sample_size
+	#A simple example
+	example(sample_size)
+
+<a name="usage"/>
+# Usage #
+
+In RnaSeqSampleSize package, sample_size is the primary function to estimate sample size based on different parameters.
+	
+	#List of all possible parameters for sample_size function:
+	# m: Total number of genes for testing.
+	# m1: Expected number of prognostic genes.
+	# power: Power to detecte prognostic genes.
+	# f: FDR level
+	# w: Ratio of normalization factors between two groups.
+	# rho: minimum fold changes for prognostic genes between two groups.
+	# lambda0: Average read counts for prognostic genes.
+	# phi_0: Dispersion for prognostic genes.
+
+	#Set the parameters:
+	vm<-10000;vm1<-100;vpower<-0.8;vf<-0.05;vw<-1.0;vrho<-2.0;vlambda0<-5;vphi_0<-0.5
+	#Estitamete sample size
+	sample_size(m=vm, m1=vm1, power=vpower, f=vf, w=vw, rho=vrho, lambda0=vlambda0, phi_0=vphi_0)
